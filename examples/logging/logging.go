de1766d7ea337da193f15045aef7728e7488b255
malklera <malklerulo@gmail.com>
2026-05-10 10:10:13 -0300

    feat: add multiple output to logging
    Show how to add output to Stderr, a file, and an outside command, in
    this case a notification daemon.

--- examples/logging/logging.go
+++ examples/logging/logging.go
@@ -9,14 +9,55 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"log/slog"
 )
 
-func main() {
+// NotifyWriter is a placeholder to satisfy the `Writer` for the log notification
+type NotifyWriter struct{}
 
+// Write sends p to the notify-send cli
+func (nw NotifyWriter) Write(p []byte) (n int, err error) {
+	cmd := exec.Command("notify-send", "fci", string(p))
+
+	// This is for capturing any error returned by the command
+	var errBuf bytes.Buffer
+	cmd.Stderr = &errBuf
+
+	// Run instead of Start to wait for the return of the command
+	err = cmd.Run()
+	if err != nil {
+		// First I try to write to log, but that created a recursion loop, because
+		// it try to write to the command, which error, and repeat, instead
+		// write to `os.Stderr`
+		fmt.Fprintf(os.Stderr, "Failed to send notification : %v", err)
+		return 0, err
+	}
+	return len(p), nil
+}
+
+// setLogs takes a path and set the different output for logs, `envDir/file`,
+// `os.Stderr` and some notification command.
+func setLogs(envDir string) (*os.File, io.Writer) {
+	nw := NotifyWriter{}
+	multiWriter := io.MultiWriter(os.Stderr, nw)
+	logPath := filepath.Join(envDir, "test-logs.log")
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("os.OpenFile(%s, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) : %v", logPath, err)
+		return nil, multiWriter
+	} else {
+		multiWriter = io.MultiWriter(os.Stderr, logFile, nw)
+	}
+	return logFile, multiWriter
+}
+
+func main() {
 	// Simply invoking functions like `Println` from the
 	// `log` package uses the _standard_ logger, which
 	// is already pre-configured for reasonable logging
@@ -74,4 +115,15 @@ func main() {
 	// contain an arbitrary number of key=value
 	// pairs.
 	myslog.Info("hello again", "key", "val", "age", 25)
+
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("Output logs to os.Stderr, a test-logs.log and notify-send")
+	logFile, multiWriter := setLogs(".")
+	defer logFile.Close()
+
+	log.SetOutput(multiWriter)
+
+	log.Printf("test with printf1")
 }
