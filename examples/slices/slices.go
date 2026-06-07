c1b44f6b44b09a1d9789385e6dc54c0d679f5b35
malklera <malklerulo@gmail.com>
2026-06-07 11:14:01 -0300

    feat: Change var names and add help output slices

--- examples/slices/slices.go
+++ examples/slices/slices.go
@@ -14,8 +14,11 @@ func main() {
 	// elements they contain (not the number of elements).
 	// An uninitialized slice equals to nil and has
 	// length 0.
-	var s []string
-	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+	var unInit []string
+	fmt.Println("var unInit []string:", unInit)
+	fmt.Println("unInit == nil:", unInit == nil)
+	fmt.Println("len(unInit):", len(unInit))
+	fmt.Println()
 
 	// To create a slice with non-zero length, use
 	// the builtin `make`. Here we make a slice of
@@ -24,18 +27,27 @@ func main() {
 	// length; if we know the slice is going to grow ahead
 	// of time, it's possible to pass a capacity explicitly
 	// as an additional parameter to `make`.
-	s = make([]string, 3)
-	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+	slice3 := make([]string, 3)
+	fmt.Println("slice3 := make([]string, 3):", slice3)
+	fmt.Println("len(slice3):", len(slice3))
+	fmt.Println("cap(slice3):", cap(slice3))
+	fmt.Println()
 
 	// We can set and get just like with arrays.
-	s[0] = "a"
-	s[1] = "b"
-	s[2] = "c"
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
+	slice3[0] = "a"
+	slice3[1] = "b"
+	slice3[2] = "c"
+	fmt.Println("slice3[0] = \"a\"")
+	fmt.Println("slice3[1] = \"b\"")
+	fmt.Println("slice3[2] = \"c\"")
+
+	fmt.Println("slice3:", slice3)
+	fmt.Println("slice3[2]:", slice3[2])
 
 	// `len` returns the length of the slice as expected.
-	fmt.Println("len:", len(s))
+	fmt.Println("len(slice3):", len(slice3))
+	fmt.Println("cap(slice3):", cap(slice3))
+	fmt.Println()
 
 	// In addition to these basic operations, slices
 	// support several more that make them richer than
@@ -43,42 +55,58 @@ func main() {
 	// returns a slice containing one or more new values.
 	// Note that we need to accept a return value from
 	// `append` as we may get a new slice value.
-	s = append(s, "d")
-	s = append(s, "e", "f")
-	fmt.Println("apd:", s)
+	fmt.Println("slice3:", slice3)
+	slice3 = append(slice3, "d")
+	fmt.Println("slice3 = append(slice3, \"d\")")
+	fmt.Println("slice3:", slice3)
+	slice3 = append(slice3, "e", "f")
+	fmt.Println("slice3 = append(slice3, \"e\", \"f\")")
+	fmt.Println("slice3:", slice3)
+	fmt.Println()
 
 	// Slices can also be `copy`'d. Here we create an
-	// empty slice `c` of the same length as `s` and copy
-	// into `c` from `s`.
-	c := make([]string, len(s))
-	copy(c, s)
-	fmt.Println("cpy:", c)
+	// empty slice `copySlice` of the same length as `s` and copy
+	// into `copySlice` from `s`.
+	copySlice := make([]string, len(slice3))
+	fmt.Println("copySlice:", copySlice)
+	fmt.Println("copy(copySlice, slice3)")
+	copy(copySlice, slice3)
+	fmt.Println("copySlice:", copySlice)
+	fmt.Println()
 
 	// Slices support a "slice" operator with the syntax
 	// `slice[low:high]`. For example, this gets a slice
 	// of the elements `s[2]`, `s[3]`, and `s[4]`.
-	l := s[2:5]
-	fmt.Println("sl1:", l)
+	sliceLowHigh := slice3[2:5]
+	fmt.Println("sliceLowHigh := slice3[2:5]")
+	fmt.Println("sliceLowHigh:", sliceLowHigh)
+	fmt.Println()
 
 	// This slices up to (but excluding) `s[5]`.
-	l = s[:5]
-	fmt.Println("sl2:", l)
+	sliceHigh := slice3[:5]
+	fmt.Println("sliceHigh := slice3[:5]")
+	fmt.Println("sliceHigh:", sliceHigh)
+	fmt.Println()
 
 	// And this slices up from (and including) `s[2]`.
-	l = s[2:]
-	fmt.Println("sl3:", l)
+	sliceLow := slice3[2:]
+	fmt.Println("sliceLow := slice3[2:]")
+	fmt.Println("sliceLow:", sliceLow)
+	fmt.Println()
 
 	// We can declare and initialize a variable for slice
 	// in a single line as well.
-	t := []string{"g", "h", "i"}
-	fmt.Println("dcl:", t)
+	sliceInit := []string{"g", "h", "i"}
+	fmt.Println("sliceInit := []string{\"g\", \"h\", \"i\"}")
+	fmt.Println("sliceInit:", sliceInit)
+	fmt.Println()
 
 	// The `slices` package contains a number of useful
 	// utility functions for slices.
-	t2 := []string{"g", "h", "i"}
-	if slices.Equal(t, t2) {
-		fmt.Println("t == t2")
-	}
+	secondInit := []string{"g", "h", "i"}
+	fmt.Println("secondInit := []string{\"g\", \"h\", \"i\"}")
+	fmt.Println("slices.Equal(sliceInit, secondInit):", slices.Equal(sliceInit, secondInit))
+	fmt.Println()
 
 	// Slices can be composed into multi-dimensional data
 	// structures. The length of the inner slices can
@@ -91,5 +119,5 @@ func main() {
 			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoD)
+	fmt.Println("twoD: ", twoD)
 }
