6501cfa713cde4575cc89d1d4334b4852d927e00
malklera <malklerulo@gmail.com>
2026-06-06 21:21:49 -0300

    feat: make reading-files self contained
    Create and populate the file used in the readings.
    
    Change name of variables for better clarity.
    
    Add clarifications in the outputs.

--- examples/reading-files/reading-files.go
+++ examples/reading-files/reading-files.go
@@ -21,74 +21,94 @@ func check(e error) {
 }
 
 func main() {
-
+	path := filepath.Join(os.TempDir(), "data")
+	dataFile, err := os.Create(path)
+	check(err)
+	_, err = dataFile.WriteString("hello\ngo\n")
+	check(err)
 	// Perhaps the most basic file reading task is
 	// slurping a file's entire contents into memory.
-	path := filepath.Join(os.TempDir(), "dat")
-	dat, err := os.ReadFile(path)
+	content, err := os.ReadFile(path)
 	check(err)
-	fmt.Print(string(dat))
+	fmt.Printf("os.ReadFile(%s)\n", path)
+	fmt.Print(string(content))
+	fmt.Println()
 
 	// You'll often want more control over how and what
 	// parts of a file are read. For these tasks, start
 	// by `Open`ing a file to obtain an `os.File` value.
-	f, err := os.Open(path)
+	file, err := os.Open(path)
 	check(err)
 
 	// Read some bytes from the beginning of the file.
 	// Allow up to 5 to be read but also note how many
 	// actually were read.
-	b1 := make([]byte, 5)
-	n1, err := f.Read(b1)
+	byte5 := make([]byte, 5)
+	read5, err := file.Read(byte5)
 	check(err)
-	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+	fmt.Printf("file.Read(make([]byte, 5))\n")
+	fmt.Printf("read '%d' bytes: '%s'\n", read5, string(byte5[:read5]))
+	fmt.Println()
 
 	// You can also `Seek` to a known location in the file
 	// and `Read` from there.
-	o2, err := f.Seek(6, io.SeekStart)
+	seek6, err := file.Seek(6, io.SeekStart)
 	check(err)
-	b2 := make([]byte, 2)
-	n2, err := f.Read(b2)
+	byte2 := make([]byte, 2)
+	read2, err := file.Read(byte2)
 	check(err)
-	fmt.Printf("%d bytes @ %d: ", n2, o2)
-	fmt.Printf("%v\n", string(b2[:n2]))
+	fmt.Println("file.Seek(6, io.SeekStart)")
+	fmt.Println("file.Read(make([]byte, 2))")
+	fmt.Printf("read '%d' bytes from '%d': '%s'\n", read2, seek6, string(byte2[:read2]))
+	fmt.Println()
 
 	// Other methods of seeking are relative to the
 	// current cursor position,
-	_, err = f.Seek(2, io.SeekCurrent)
+	_, err = file.Seek(2, io.SeekCurrent)
 	check(err)
 
 	// and relative to the end of the file.
-	_, err = f.Seek(-4, io.SeekEnd)
+	_, err = file.Seek(-4, io.SeekEnd)
 	check(err)
+
+	// TODO: should make two examples that actually show the difference between
+	// `Read` and `io.ReadAtLeast`
 
 	// The `io` package provides some functions that may
 	// be helpful for file reading. For example, reads
 	// like the ones above can be more robustly
 	// implemented with `ReadAtLeast`.
-	o3, err := f.Seek(6, io.SeekStart)
+	seekAtLeast6, err := file.Seek(6, io.SeekStart)
 	check(err)
-	b3 := make([]byte, 2)
-	n3, err := io.ReadAtLeast(f, b3, 2)
+	byteAtLeast2 := make([]byte, 2)
+	readAtLeast2, err := io.ReadAtLeast(file, byteAtLeast2, 2)
 	check(err)
-	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+	fmt.Println("file.Seek(6, io.SeekStart)")
+	fmt.Println("file.Read(make([]byte, 2))")
+	fmt.Printf("read '%d' bytes from '%d': '%s'\n", readAtLeast2, seekAtLeast6, string(byteAtLeast2))
+	fmt.Println()
 
 	// There is no built-in rewind, but
 	// `Seek(0, io.SeekStart)` accomplishes this.
-	_, err = f.Seek(0, io.SeekStart)
+	_, err = file.Seek(0, io.SeekStart)
 	check(err)
 
 	// The `bufio` package implements a buffered
 	// reader that may be useful both for its efficiency
 	// with many small reads and because of the additional
 	// reading methods it provides.
-	r4 := bufio.NewReader(f)
-	b4, err := r4.Peek(5)
+	bufioReader := bufio.NewReader(file)
+	peek5, err := bufioReader.Peek(5)
 	check(err)
-	fmt.Printf("5 bytes: %s\n", string(b4))
+
+	fmt.Println("file.Seek(0, io.SeekStart)")
+	fmt.Println("bufio.NewReader(file)")
+	fmt.Println("bufioReader.Peek(5)")
+	fmt.Printf("read '5' bytes: '%s'\n", string(peek5))
+	fmt.Println()
 
 	// Close the file when you're done (usually this would
 	// be scheduled immediately after `Open`ing with
 	// `defer`).
-	f.Close()
+	file.Close()
 }
