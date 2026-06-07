a3dbf724d92f6dbe6298091610e7f3cc1991b190
malklera <malklerulo@gmail.com>
2026-06-07 11:36:26 -0300

    feat: change var names and add output help

--- examples/maps/maps.go
+++ examples/maps/maps.go
@@ -12,62 +12,71 @@ func main() {
 
 	// To create an empty map, use the builtin `make`:
 	// `make(map[key-type]val-type)`.
-	m := make(map[string]int)
+	makeMap := make(map[string]int)
+	fmt.Println("makeMap := make(map[string]int)")
+	fmt.Println("makeMap:", makeMap)
 
 	// Set key/value pairs using typical `name[key] = val`
 	// syntax.
-	m["k1"] = 7
-	m["k2"] = 13
+	makeMap["k1"] = 7
+	makeMap["k2"] = 13
 
 	// Printing a map with e.g. `fmt.Println` will show all of
 	// its key/value pairs.
-	fmt.Println("m:", m)
+	fmt.Println("makeMap[\"k1\"] = 7")
+	fmt.Println("makeMap[\"k2\"] = 13")
+	fmt.Println("makeMap:", makeMap)
 
 	// Get a value for a key with `name[key]`.
-	v1 := m["k1"]
-	fmt.Println("v1:", v1)
+	fmt.Println("makeMap[\"k1\"]:", makeMap["k1"])
 
 	// If the key doesn't exist, the
 	// [zero value](https://go.dev/ref/spec#The_zero_value) of the
 	// value type is returned.
-	v3 := m["k3"]
-	fmt.Println("v3:", v3)
+	fmt.Println("makeMap[\"k3\"]:", makeMap["k3"])
 
 	// The builtin `len` returns the number of key/value
 	// pairs when called on a map.
-	fmt.Println("len(m):", len(m))
+	fmt.Println("len(makeMap):", len(makeMap))
+	fmt.Println()
 
 	// The builtin `delete` removes key/value pairs from
 	// a map.
-	fmt.Println("m:", m)
-	delete(m, "k2")
-	fmt.Println("delete(m, 'k2'):", m)
+	fmt.Println("makeMap:", makeMap)
+	delete(makeMap, "k2")
+	fmt.Println("delete(makeMap, 'k2')")
+	fmt.Println("makeMap:", makeMap)
+	fmt.Println()
 
 	// To remove *all* key/value pairs from a map, use
 	// the `clear` builtin.
-	fmt.Println("map:", m)
-	clear(m)
-	fmt.Println("clear(m):", m)
+	fmt.Println("makeMap:", makeMap)
+	clear(makeMap)
+	fmt.Println("clear(makeMap)")
+	fmt.Println("makeMap:", makeMap)
+	fmt.Println()
 
 	// The optional second return value when getting a
 	// value from a map indicates if the key was present
 	// in the map. This can be used to disambiguate
 	// between missing keys and keys with zero values
-	// like `0` or `""`. Here we didn't need the value
-	// itself, so we ignored it with the _blank identifier_
-	// `_`.
-	_, prs := m["k2"]
-	fmt.Println("prs:", prs)
+	// like `0` or `""`.
+	secondKey, exist := makeMap["k2"]
+	fmt.Println("secondKey, exist := makeMap[\"k2\"]")
+	fmt.Println("secondKey:", secondKey)
+	fmt.Println("exist:", exist)
+	fmt.Println()
 
 	// You can also declare and initialize a new map in
 	// the same line with this syntax.
-	n := map[string]int{"foo": 1, "bar": 2}
-	fmt.Println("map:", n)
+	mapInit := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("mapInit := map[string]int{\"foo\": 1, \"bar\": 2}")
+	fmt.Println("mapInit:", mapInit)
+	fmt.Println()
 
 	// The `maps` package contains a number of useful
 	// utility functions for maps.
-	n2 := map[string]int{"foo": 1, "bar": 2}
-	if maps.Equal(n, n2) {
-		fmt.Println("n == n2")
-	}
+	secondInit := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("secondInit := map[string]int{\"foo\": 1, \"bar\": 2}")
+	fmt.Println("maps.Equal(mapInit, secondInit):", maps.Equal(mapInit, secondInit))
 }
