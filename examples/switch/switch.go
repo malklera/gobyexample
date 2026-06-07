de464f7a7d825eedabb289c4ec4ccc160b52fae5
malklera <malklerulo@gmail.com>
2026-06-07 09:23:09 -0300

    feat: add some line jump, change some value
    Change the values of some variables to go to different cases in
    different examples.

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -22,6 +22,7 @@ func main() {
 	case 3:
 		fmt.Println("three")
 	}
+	fmt.Println()
 
 	// You can use commas to separate multiple expressions
 	// in the same `case` statement. We use the optional
@@ -32,6 +33,7 @@ func main() {
 	default:
 		fmt.Println("It's a weekday")
 	}
+	fmt.Println()
 
 	// `switch` without an expression is an alternate way
 	// to express if/else logic. Here we also show how the
@@ -43,6 +45,7 @@ func main() {
 	default:
 		fmt.Println("It's after noon")
 	}
+	fmt.Println()
 
 	// A type `switch` compares types instead of values.  You
 	// can use this to discover the type of an interface
@@ -58,40 +61,46 @@ func main() {
 			fmt.Printf("Don't know type %T\n", t)
 		}
 	}
+	fmt.Print("whatAmI(true): ")
 	whatAmI(true)
+	fmt.Print("whatAmI(1): ")
 	whatAmI(1)
+	fmt.Print("whatAmI(hey): ")
 	whatAmI("hey")
+	fmt.Println()
 
 	// This evaluate each case till it hit a true or default, stoping at the first
 	// true, all the cases are excluyent to each other
-	someString := "bc"
+	someString := "ab"
 	switch {
-	case someString == "b":
-		fmt.Println("someString == b")
-	case strings.EqualFold(someString, "b"):
-		fmt.Println("strings.EqualFold(someString == b)")
+	case someString == "a":
+		fmt.Println("someString == a")
+	case strings.EqualFold(someString, "a"):
+		fmt.Println("strings.EqualFold(someString, a)")
 	default:
-		fmt.Println("default someString := bc")
+		fmt.Println("default someString := ab")
 	}
+	fmt.Println()
 
 	// If testing the same type for different values, you can declare it inline
-	switch otherString := "bc"; otherString {
-	case "b":
-		fmt.Println("otherString == b")
-	case "bc":
-		fmt.Println("otherString == bc")
+	switch otherString := "cd"; otherString {
+	case "c":
+		fmt.Println("otherString == c")
+	case "cd":
+		fmt.Println("otherString == cd")
 	default:
-		fmt.Println("default otherString := bc")
+		fmt.Println("default otherString := cd")
 	}
+	fmt.Println()
 
 	// or use one declared in another place.
-	thirdString := "bc"
+	thirdString := "e"
 	switch thirdString {
-	case "b":
-		fmt.Println("thirdString == b")
-	case "bc":
-		fmt.Println("thirdString == bc")
+	case "e":
+		fmt.Println("thirdString == e")
+	case "ef":
+		fmt.Println("thirdString == ef")
 	default:
-		fmt.Println("default thirdString := bc")
+		fmt.Println("default thirdString := ef")
 	}
 }
