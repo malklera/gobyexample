6cd7788a87b31805ff57dba52c084d8042fb8641
malklera <malklerulo@gmail.com>
2026-06-06 22:46:50 -0300

    feat: update variable names and output
    Change variables names to be more descriptive.
    
    Add more clarification in the outputs messages.
    
    Change the `twoDInit` content to be the same as `twoD`.

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -9,37 +9,45 @@ import "fmt"
 
 func main() {
 
-	// Here we create an array `a` that will hold exactly
+	// Here we create an array `a5` that will hold exactly
 	// 5 `int`s. The type of elements and length are both
 	// part of the array's type. By default an array is
 	// zero-valued, which for `int`s means `0`s.
-	var a [5]int
-	fmt.Println("emp:", a)
+	var a5 [5]int
+	fmt.Println("a5:", a5)
 
 	// We can set a value at an index using the
 	// `array[index] = value` syntax, and get a value with
 	// `array[index]`.
-	a[4] = 100
-	fmt.Println("set:", a)
-	fmt.Println("get:", a[4])
+	a5[4] = 100
+	fmt.Println("a5[4] = 100")
+	fmt.Println("a5:", a5)
+	fmt.Println("a5[4]:", a5[4])
 
 	// The builtin `len` returns the length of an array.
-	fmt.Println("len:", len(a))
+	fmt.Println("len(a5):", len(a5))
+	fmt.Println()
 
 	// Use this syntax to declare and initialize an array
 	// in one line.
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
+	b5 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("b5 := [5]int{1, 2, 3, 4, 5}")
+	fmt.Println("b5:", b5)
+	fmt.Println()
 
 	// You can also have the compiler count the number of
 	// elements for you with `...`
-	b = [...]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
+	compCount := [...]int{1, 2, 3, 4, 5}
+	fmt.Println("compCount := [...]int{1, 2, 3, 4, 5}")
+	fmt.Println("compCount:", compCount)
+	fmt.Println()
 
 	// If you specify the index with `:`, the elements in
 	// between will be zeroed.
-	b = [...]int{100, 3: 400, 500}
-	fmt.Println("idx:", b)
+	compCountIndex := [...]int{100, 3: 400, 500}
+	fmt.Println("compCountIndex := [...]int{100, 3: 400, 500}")
+	fmt.Println("compCountIndex:", compCountIndex)
+	fmt.Println()
 
 	// Array types are one-dimensional, but you can
 	// compose types to build multi-dimensional data
@@ -50,13 +58,14 @@ func main() {
 			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoD)
+	fmt.Println("twoD: ", twoD)
+	fmt.Println()
 
 	// You can create and initialize multi-dimensional
 	// arrays at once too.
-	twoD = [2][3]int{
-		{1, 2, 3},
+	twoDInit := [2][3]int{
+		{0, 1, 2},
 		{1, 2, 3},
 	}
-	fmt.Println("2d: ", twoD)
+	fmt.Println("twoDInit: ", twoDInit)
 }
